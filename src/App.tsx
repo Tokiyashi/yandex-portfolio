49e93745dc97855b515fb90010546658d7c28a5b
Nikita Rogovskiy <rogich6319@gmail.com>
2023-08-26 00:40:48 +0300

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
       <Routes>
-        <Route path='/*' element={<Main/>}/>
+        <Route path='/' element={<Main/>}/>
         <Route path='/guess-the-number' element={<GuessTheNumber/>}/>
       </Routes>
   )
