ba9630662b39524d63ac048e572ea007a90809d3
Nikita Rogovskiy <rogich6319@gmail.com>
2023-08-26 01:12:56 +0300

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return
+        }
+        warn(warning)
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
